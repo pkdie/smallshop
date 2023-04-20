46b2e51069a460dcb257c0ba13dc30c2a4fec138
pkdie <intel760@naver.com>
2023-04-20 21:44:37 +0900

    smallShop applicationTestd에서 @SpringBootTest를 주석 처리

--- src/test/java/com/project/smallshop/SmallshopApplicationTests.java
+++ src/test/java/com/project/smallshop/SmallshopApplicationTests.java
@@ -3,7 +3,7 @@ package com.project.smallshop;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+//@SpringBootTest
 class SmallshopApplicationTests {
 
 	@Test
