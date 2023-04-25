4af983bd42c927db39518a6cb89af55946ab8fb7
tt <intel760@naver.com>
2023-04-25 10:50:23 +0900

    smallShop

--- src/main/java/com/project/smallshop/Config/S3Config.java
+++ src/main/java/com/project/smallshop/Config/S3Config.java
@@ -1,4 +1,8 @@
 package com.project.smallshop.Config;
 
+import org.springframework.beans.factory.annotation.Value;
+
 public class S3Config {
+    @Value("${cloud.aws.credentials.accessKey}")
+    private String accessKey;
 }
