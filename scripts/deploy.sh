a37750d9776b9a862b1d08a3ea2328e7ec40a8f1
tt <intel760@naver.com>
2023-04-25 10:12:04 +0900

    smallShop

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -28,7 +28,7 @@ chmod +x $JAR_NAME
 echo "> $JAR_NAME 실행"
 nohup java -jar \
         -Dspring.config.location=classpath:/application.properties,/home/ubuntu/app/application-aws.properties \
-        -Dspring.profiles.active=ubuntu \
+        -Dspring.profiles.active=real \
         $JAR_NAME > $REPOSITORY/nohup.out 2>&1 &
 #-Dspring.config.location=classpath:/application.properties,classpath:/application-
 #real.properties,/home/ubuntu/app/application-oauth.properties,/home/ubuntu/app/application-real-
